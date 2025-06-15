f7a4d844e7e01c7446d570164d27fe45e5603ccc
hikartik <kartik.200904@gmail.com>
2025-06-16 00:52:12 +0530

    Installing dependencies

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name='cellSegmentation',
+    version='0.0.0',
+    author='Kartik Kumar',
+    author_email='kartik.200904@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
